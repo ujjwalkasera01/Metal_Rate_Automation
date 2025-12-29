75f743b7299d6f04390718c5f7ad7f1eb259ebcb
ujjwalkasera <itsujjwalkasera@gmail.com>
2025-12-29 19:31:12 +0530

    changes

--- api/app.py
+++ api/app.py
@@ -77,4 +77,6 @@ async def webhook(request: Request):
 
 # ---------------- LOCAL RUN SUPPORT ----------------
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=8000)
+    import uvicorn, os
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run("api.app:app", host="0.0.0.0", port=port)
